f3af9ed34dd59b5de87592ed6ce4d94de93fc02c
Andrey Lisin <andrey.lisin@gmail.com>
2016-09-07 00:02:16 +0600

    Handle compilation errors gracefully

--- src/clojureEval.ts
+++ src/clojureEval.ts
@@ -22,11 +22,11 @@ export function clojureEval(context: vscode.ExtensionContext) {
 
     let editor = vscode.window.activeTextEditor;
     let text: string = editor.document.getText();
-    let ns: string = text.match(/^.*\((?:[\s\t\n]*(?:in-){0,1}ns)[\s\t\n]+'?(\w+)[\s\S]*\)[\s\S]*/)[1] || 'user';
-    let isSelection = !editor.selection.isEmpty;
+    let ns: string = text.match(/^.*\((?:[\s\t\n]*(?:in-){0,1}ns)[\s\t\n]+'?([\w.\-\/]+)[\s\S]*\)[\s\S]*/)[1] || 'user';
+    let selection = editor.selection;
+    let isSelection = !selection.isEmpty;
 
     if (isSelection) {
-        let selection = editor.selection;
         text = `(ns ${ns}) ${editor.document.getText(selection)}`;
     }
 
@@ -40,59 +40,41 @@ export function clojureEval(context: vscode.ExtensionContext) {
 
     let filename = editor.document.fileName;
 
-    let nrepl = new nREPLClient(port, host);
+    let nrepl1 = new nREPLClient(port, host);
     let diagnostics = vscode.languages.createDiagnosticCollection('Compilation Errors');
     diagnostics.clear();
-    nrepl.evalFile(text, filename, (result) => {
+    nrepl1.evalFile(text, filename, (result) => {
         console.log(result);
         if (result.value) {
             vscode.window.showInformationMessage('Successfully compiled')
             diagnostics.clear();
-        } else if (result.status) {
-            nrepl.stacktrace(result.session, (stackteace) => {
+        } else if (result.ex) {
+            let nrepl2 = new nREPLClient(port, host);
+            nrepl2.stacktrace(result.session, (stackteace) => {
                 vscode.window.showErrorMessage('Compilation error');
-                let errorDescription = parseError(stackteace.err);
-                let errLine = errorDescription.position.line;
-                let errChar = errorDescription.position.character;
-                let errMsg = errorDescription.message;
-                editor.selection = new vscode.Selection(errorDescription.position, errorDescription.position);
+                let errLine = stackteace.line - 1;
+                let errChar = stackteace.column;
+                let errFile = stackteace.file;
+                let errFileUri: vscode.Uri;
+                if (errFile) {
+                    errFileUri = vscode.Uri.parse('file://' + errFile);
+                } else {
+                    errFileUri = vscode.window.activeTextEditor.document.uri;
+                }
+                let errMsg = stackteace.message;
+
+                // Adjust error position if selection has been evaluated
+                if (isSelection) {
+                    errLine = errLine + selection.start.line;
+                    errChar = errChar + selection.start.character;
+                }
+
+                let errPos = new vscode.Position(errLine, errChar);
+                editor.selection = new vscode.Selection(errPos, errPos);
                 let errLineLength = editor.document.lineAt(errLine).text.length;
-                diagnostics.set(vscode.window.activeTextEditor.document.uri, [new vscode.Diagnostic(new vscode.Range(errLine, 0, errLine, errLineLength), errMsg, vscode.DiagnosticSeverity.Error)]);
+
+                diagnostics.set(errFileUri, [new vscode.Diagnostic(new vscode.Range(errLine, 1, errLine, errLineLength), errMsg, vscode.DiagnosticSeverity.Error)]);
             })
         }
     })
-}
-
-function parseError(error: string): ErrorDescription {
-    let m = error.match(/(\d+):(\d+)/);
-    if (m) {
-        let [line, char] = [Number.parseInt(m[1]), Number.parseInt(m[2])];
-        return {
-            position: new vscode.Position(line - 1, char),
-            message: error
-        }
-    } else {
-        // TODO
-    }
-}
-
-function getNREPLPort(): number {
-    let nreplPort: number;
-    let projectDir = vscode.workspace.rootPath;
-    let globalNREPLFile = path.join(os.homedir(), '.lein', 'repl-port');
-
-    if (!projectDir) {
-        nreplPort = Number.parseInt(fs.readFileSync(globalNREPLFile, 'utf-8'))
-    }
-
-    if (projectDir) {
-        let localNREPLFile = path.join(projectDir, '.nrepl-port');
-        if (fs.existsSync(localNREPLFile)) {
-            nreplPort = Number.parseInt(fs.readFileSync(localNREPLFile, 'utf-8'))
-        } else {
-            nreplPort = Number.parseInt(fs.readFileSync(globalNREPLFile, 'utf-8'))
-        }
-    }
-
-    return nreplPort;
 }

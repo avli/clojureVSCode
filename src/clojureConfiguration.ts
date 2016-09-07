7d9f76f15a9b3b408fedf5c9f1339664252eea3c
Andrey Lisin <andrey.lisin@gmail.com>
2016-09-07 17:08:21 +0600

    Add indentation rules

--- src/clojureConfiguration.ts
+++ src/clojureConfiguration.ts
@@ -2,12 +2,77 @@
 
 import * as vscode from 'vscode';
 
+const keywords = [
+    'ns',
+    'fn',
+    'def',
+    'defn',
+    'bound\\-fn',
+    'if',
+    'if\\-not',
+    'case,',
+    'cond',
+    'condp',
+    'cond\\-\\>',
+    'cond\\-\\>\\>',
+    'when',
+    'while',
+    'when\\-not',
+    'when\\-first',
+    'do',
+    'future',
+    'comment',
+    'doto',
+    'locking',
+    'proxy',
+    'as\\-\\>',
+    'reify',
+    'deftype',
+    'defrecord',
+    'defprotocol',
+    'extend',
+    'extend-protocol',
+    'extend-type',
+    'specify',
+    'specify\\!',
+    'try',
+    'catch',
+    'finally',
+    'let',
+    'letfn',
+    'binding',
+    'loop',
+    'for',
+    'doseq',
+    'dotimes',
+    'when\\-let',
+    'if\\-let',
+    'when\\-some',
+    'if\\-some',
+    'this\\-as',
+    'defmethod',
+    'testing',
+    'deftest',
+    'are',
+    'use\\-fixtures',
+    'run',
+    'run\\*',
+    'fresh',
+    'alt!',
+    'alt!!',
+    'go',
+    'go\\-loop',
+    'thread'
+].join('|');
+
+const pattern = new RegExp(`^[\\s\\t]*?\\([\\s\\t]*${keywords}.*$`);
+
 export class ClojureLanguageConfiguration implements vscode.LanguageConfiguration {
     wordPattern = /[\w\-\.][\w\d\.\\/\-\?]+/;
-    onEnterRules = [
-            {
-                beforeText: /^\s*(?:def|defn|for|if|do|else|while|try).*\s*\)?\s*$/,
-                action: { indentAction: vscode.IndentAction.Indent }
-            }
-        ]
+    onEnterRules = [{
+        beforeText: pattern,
+        action: {
+            indentAction: vscode.IndentAction.Indent
+        }
+    }]
 }

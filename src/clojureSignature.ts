498dbb5e0a89e021ca98ef414168a6af31f27a3c
fasfsfgs <fasfsfgs@gmail.com>
2017-01-08 19:44:38 -0200

    signature helper for special forms

--- src/clojureSignature.ts
+++ src/clojureSignature.ts
@@ -8,6 +8,8 @@ import * as cljParser from './cljParser';
 const PARAMETER_OPEN = `[`;
 const PARAMETER_CLOSE = `]`;
 const PARAMETER_REST = `&`;
+const UNSUPPORTED_SIGNATURE_NAMES = ['.', 'new', 'fn', 'set!']; // they have forms that do not follow the same rules as other special forms
+const SPECIAL_FORM_PARAMETER_REST = `*`;
 
 export class ClojureSignatureProvider extends ClojureProvider implements vscode.SignatureHelpProvider {
 
@@ -21,23 +23,58 @@ export class ClojureSignatureProvider extends ClojureProvider implements vscode.
 
         return new Promise<vscode.SignatureHelp>((resolve, reject) => {
             this.getNREPL().info(exprInfo.functionName, ns, info => {
-                resolve(getSignatureHelp(info, exprInfo.parameterPosition));
+                if (!info.name) // sometimes info brings just a list of suggestions (example: .MAX_VALUE)
+                    return resolve();
+
+                if (!!info['special-form'])
+                    return resolve(getSpecialFormSignatureHelp(info, exprInfo.parameterPosition));
+
+                return resolve(getFunctionSignatureHelp(info, exprInfo.parameterPosition));
             });
         });
     }
 
 }
 
-function getSignatureHelp(info, parameterPosition): vscode.SignatureHelp {
-    const signatures = getSignatureInfos(info);
+function getSpecialFormSignatureHelp(info: any, parameterPosition: number): vscode.SignatureHelp {
+    if (UNSUPPORTED_SIGNATURE_NAMES.indexOf(info.name) > -1) {
+        const signatureHelp = new vscode.SignatureHelp();
+        signatureHelp.signatures = [new vscode.SignatureInformation(`${info.name} *special form* ${info['forms-str']}`, info.doc)];
+        signatureHelp.activeSignature = 0;
+
+        return signatureHelp;
+    }
+
+    const forms: string = info['forms-str'];
+    const [functionName, ...parameters] = forms.substring(3, forms.length - 1).split(' ');
+    const parameterInfos = parameters.map(parameter => new vscode.ParameterInformation(parameter));
+
+    const sigInfo = new vscode.SignatureInformation(`${info.name} *special form* [${parameterInfos.map(pi => pi.label).join(`\n`)}]`, info.doc);
+    sigInfo.parameters = parameterInfos;
+
+    if (parameterPosition + 1 > sigInfo.parameters.length && sigInfo.parameters[sigInfo.parameters.length - 1].label.endsWith(SPECIAL_FORM_PARAMETER_REST))
+        parameterPosition = sigInfo.parameters.length - 1;
+
+    const signatureHelp = new vscode.SignatureHelp();
+    signatureHelp.signatures = [sigInfo];
+    signatureHelp.activeParameter = parameterPosition;
+    signatureHelp.activeSignature = 0;
+
+    return signatureHelp;
+}
+
+function getFunctionSignatureHelp(info: any, parameterPosition: number): vscode.SignatureHelp {
+    const signatures = getFunctionSignatureInfos(info);
     signatures.sort((sig1, sig2) => sig1.parameters.length - sig2.parameters.length);
 
     let activeSignature = signatures.findIndex(signature => signature.parameters.length >= parameterPosition + 1);
     if (activeSignature === -1) {
         activeSignature = signatures.findIndex(signature => signature.parameters.some(param => param.label.startsWith(PARAMETER_REST)));
-        if (activeSignature != -1)
+        if (activeSignature !== -1)
             parameterPosition = signatures[activeSignature].parameters.length - 1;
     }
+    if (activeSignature === -1)
+        activeSignature = 0;
 
     const signatureHelp = new vscode.SignatureHelp();
     signatureHelp.signatures = signatures;
@@ -47,7 +84,7 @@ function getSignatureHelp(info, parameterPosition): vscode.SignatureHelp {
     return signatureHelp;
 }
 
-function getSignatureInfos(info: any): vscode.SignatureInformation[] {
+function getFunctionSignatureInfos(info: any): vscode.SignatureInformation[] {
     const arglists: string = info['arglists-str'];
 
     const sigParamStarts: number[] = [];
@@ -69,7 +106,7 @@ function getSignatureInfos(info: any): vscode.SignatureInformation[] {
     return sigParamStarts
         .map((sigParamStart, index) => arglists.substring(sigParamStart, sigParamStops[index] + 1))
         .map(signatureParameter => {
-            const parameterInfos = getParameterInfos(signatureParameter);
+            const parameterInfos = getFunctionParameterInfos(signatureParameter);
             const sigInfo = new vscode.SignatureInformation(`${info.ns}/${info.name} [${parameterInfos.map(pi => pi.label).join(`\n`)}]`);
             sigInfo.documentation = info.doc;
             sigInfo.parameters = parameterInfos;
@@ -77,7 +114,7 @@ function getSignatureInfos(info: any): vscode.SignatureInformation[] {
         });
 }
 
-function getParameterInfos(signatureParameter: string): vscode.ParameterInformation[] {
+function getFunctionParameterInfos(signatureParameter: string): vscode.ParameterInformation[] {
     signatureParameter = signatureParameter.substring(1, signatureParameter.length - 1); // removing external brackets
     const paramStarts: number[] = [];
     const paramStops: number[] = [];
@@ -112,7 +149,5 @@ function getParameterInfos(signatureParameter: string): vscode.ParameterInformat
 
     return paramStarts
         .map((paramStart, index) => signatureParameter.substring(paramStart, paramStops[index] + 1))
-        .map(parameter => {
-            return new vscode.ParameterInformation(parameter);
-        });
+        .map(parameter => new vscode.ParameterInformation(parameter));
 }

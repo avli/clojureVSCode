4bf27fe3de82ef867f2716acd679fd13207febd3
barak liato <barakliato@gmail.com>
2017-12-24 17:06:25 +0200

    disable linting if eastwood is not available - check if it becomes available over time for some reason

--- src/clojureLintingProvider.ts
+++ src/clojureLintingProvider.ts
@@ -8,11 +8,11 @@ import { CLOJURE_MODE } from './clojureMode';
 import { handleError } from './clojureEval';
 
 interface LinterWarningResult {
-	
+
 	msg: string;
-    line: number;
-    column: number;
-    linter: string;
+	line: number;
+	column: number;
+	linter: string;
 }
 
 interface LinterErrorData {
@@ -30,44 +30,45 @@ interface LinterError {
 }
 
 interface LinterResult {
-	
+
 	err: string;
 	"err-data": LinterError;
-    warnings: LinterWarningResult[];
+	warnings: LinterWarningResult[];
 }
 
-const errorsSeverity: string[] = ["bad-arglists", 
-								  "misplaced-docstrings", 
-								  "wrong-arity", 
-								  "wrong-ns-form", 
-								  "wrong-pre-post", 
-								  "wrong-tag"];
-const warningsSeverity: string[] = [":constant-test", 
-									"def-in-def", 
-									"deprecations", 
-									"keyword-typos", 
-									"local-shadows-var",
-									"redefd-vars",
-									"suspicious-expression",
-									"suspicious-test",
-									"unused-locals",
-									"unused-meta-on-macro",
-									"unused-namespaces",
-									"unused-private-vars"];
-const infoSeverity: string[] = ["no-ns-form-found", 
-								"unlimited-use", 
-								"unused-ret-vals", 
-								"unused-ret-vals-in-try"];		
+const errorsSeverity: string[] = ["bad-arglists",
+	"misplaced-docstrings",
+	"wrong-arity",
+	"wrong-ns-form",
+	"wrong-pre-post",
+	"wrong-tag"];
+const warningsSeverity: string[] = [":constant-test",
+	"def-in-def",
+	"deprecations",
+	"keyword-typos",
+	"local-shadows-var",
+	"redefd-vars",
+	"suspicious-expression",
+	"suspicious-test",
+	"unused-locals",
+	"unused-meta-on-macro",
+	"unused-namespaces",
+	"unused-private-vars"];
+const infoSeverity: string[] = ["no-ns-form-found",
+	"unlimited-use",
+	"unused-ret-vals",
+	"unused-ret-vals-in-try"];
 
 export class ClojureLintingProvider {
-	
+
 	private outputChannel: vscode.OutputChannel;
+	private eastwoodInstalled: boolean = false;
 
 	constructor(channel: vscode.OutputChannel) {
 		this.outputChannel = channel;
 	}
 
-	private getLintCommand(ns: string): string {			
+	private getLintCommand(ns: string): string {
 		return `(do (require '[eastwood.lint])
 					(require '[clojure.data.json])					
 					(-> (eastwood.lint/lint {:namespaces ['${ns}]})
@@ -88,21 +89,21 @@ export class ClojureLintingProvider {
 										(or data))
 									 data)))
 						(clojure.data.json/write-str)))`;
-	}	
+	}
 
-	private diagnosticCollection: vscode.DiagnosticCollection;	
+	private diagnosticCollection: vscode.DiagnosticCollection;
 
-	private parseLintSuccessResponse(response: string): LinterResult {		
-		const parsedToString = JSON.parse(response);		
-		return JSON.parse(parsedToString);		
+	private parseLintSuccessResponse(response: string): LinterResult {
+		const parsedToString = JSON.parse(response);
+		return JSON.parse(parsedToString);
 	}
 
 	private getSeverity(type: string) {
-		if(errorsSeverity.indexOf(type) > 0) {
+		if (errorsSeverity.indexOf(type) > 0) {
 			return vscode.DiagnosticSeverity.Error;
-		} else if(warningsSeverity.indexOf(type) > 0) {
+		} else if (warningsSeverity.indexOf(type) > 0) {
 			return vscode.DiagnosticSeverity.Warning;
-		} else if(infoSeverity.indexOf(type) > 0) {
+		} else if (infoSeverity.indexOf(type) > 0) {
 			return vscode.DiagnosticSeverity.Information;
 		}
 		else {
@@ -110,94 +111,123 @@ export class ClojureLintingProvider {
 		}
 	}
 
-    private createDiagnosticFromLintResult(document: TextDocument, warning: LinterWarningResult): Diagnostic {
-		const blockRange = cljParser.getDirectlyBeforeBlockRange(document, warning.line, warning.column);		
-		const severity = this.getSeverity(warning.linter);		
-        return new Diagnostic(blockRange, warning.msg, severity);
-    }
+	private createDiagnosticFromLintResult(document: TextDocument, warning: LinterWarningResult): Diagnostic {
+		const blockRange = cljParser.getDirectlyBeforeBlockRange(document, warning.line, warning.column);
+		const severity = this.getSeverity(warning.linter);
+		return new Diagnostic(blockRange, warning.msg, severity);
+	}
 
 	private createDiagnosticCollectionFromLintResult(document: TextDocument, result: LinterResult): Diagnostic[] {
-		let warnings = result.warnings.map((item)=>{ return this.createDiagnosticFromLintResult(document, item); });				
-		if(result.err) {						
+		let warnings = result.warnings.map((item) => { return this.createDiagnosticFromLintResult(document, item); });
+		if (result.err) {
 			const errData = result['err-data'];
-			if(errData.data != null && document.fileName.endsWith(errData.data.file)) {
+			if (errData.data != null && document.fileName.endsWith(errData.data.file)) {
 				const startLine = errData.data.line - 1;
 				const startChar = errData.data.column - 1;
 				const endLine = errData.data['end-line'] == null ? startLine : errData.data['end-line'] - 1;
-				const endChar = errData.data['end-column'] == null ? startChar : errData.data['end-column'] - 1;				
-				warnings.push({								
+				const endChar = errData.data['end-column'] == null ? startChar : errData.data['end-column'] - 1;
+				warnings.push({
 					range: new Range(errData.data.line - 1, errData.data.column - 1, endLine, endChar),
 					message: errData.cause,
 					source: "Linter Exception",
 					severity: vscode.DiagnosticSeverity.Error,
 					code: -1
 				});
-			} else {																		
-				warnings.push({														
+			} else {
+				warnings.push({
 					range: new Range(errData.trace - 1, 0, errData.trace - 1, 0),
 					message: errData.cause,
 					source: "Linter Exception",
 					severity: vscode.DiagnosticSeverity.Error,
 					code: -1
 				});
-			}}		
+			}
+		}
 
 		return warnings;
 	}
 
-	private lint(textDocument: vscode.TextDocument) :void {		
-		if (textDocument.languageId !== CLOJURE_MODE.language || !nreplConnection.cljConnection.isConnected) {
+	private isEastwoodInstalled(textDocument: vscode.TextDocument) {
+		return nreplConnection.cljConnection
+			.sessionForFilename(textDocument.fileName)
+			.then(value => {
+				return nreplClient.evaluate("(require '[eastwood.lint])")
+					.then(result => {
+						try {
+							return result[0].ex == null;
+						} catch (e) {
+							return false;
+						}
+					}, () => false);
+			}, () => false);
+	}
+
+	private lint(textDocument: vscode.TextDocument): void {
+
+		if (!this.eastwoodInstalled) {
+			this.isEastwoodInstalled(textDocument)
+				.then(value => {
+					this.eastwoodInstalled = value || false;
+					if (this.eastwoodInstalled)
+						this.lint(textDocument);
+				});
+
+			return;
+		}
+
+		if (textDocument.languageId !== CLOJURE_MODE.language
+			|| !nreplConnection.cljConnection.isConnected
+			|| !this.eastwoodInstalled) {
 			return;
 		}
 
 		nreplConnection.cljConnection
-			 .sessionForFilename(textDocument.fileName)
-			 .then(value => {                
-                const ns = cljParser.getNamespace(textDocument.getText());                
-				if(ns.length > 0) {
-						const command = this.getLintCommand(ns);
-						nreplClient.evaluate(command)
-								   .then(result => {									   		
-											try {												
-												if(!!result[0].ex) {		
-													console.log(result);
-													handleError(this.outputChannel, 
-														new vscode.Selection(0,0,0,0), 
-														false, 
-														result[0].session);													
-												} else {																					
-													let lintResult: LinterResult = this.parseLintSuccessResponse(result[0].value);
-													const diagnostics = this.createDiagnosticCollectionFromLintResult(textDocument, lintResult);
-													this.diagnosticCollection.set(textDocument.uri, diagnostics);
-												}
-											} catch(e) {
-												console.error(e);
-											}
+			.sessionForFilename(textDocument.fileName)
+			.then(value => {
+				const ns = cljParser.getNamespace(textDocument.getText());
+				if (ns.length > 0) {
+					const command = this.getLintCommand(ns);
+					nreplClient.evaluate(command)
+						.then(result => {
+							try {
+								if (!!result[0].ex) {
+									handleError(this.outputChannel,
+										new vscode.Selection(0, 0, 0, 0),
+										false,
+										result[0].session);
+								} else {
+									let lintResult: LinterResult = this.parseLintSuccessResponse(result[0].value);
+									const diagnostics = this.createDiagnosticCollectionFromLintResult(textDocument, lintResult);
+									this.diagnosticCollection.set(textDocument.uri, diagnostics);
+								}
+							} catch (e) {
+								console.error(e);
+							}
 
-									}, err=> {
-										console.error(err);
-									});
+						}, err => {
+							console.error(err);
+						});
 				}
-             });		
+			});
 	}
 
-	public activate(subscriptions: vscode.Disposable[]) {		
+	public activate(subscriptions: vscode.Disposable[]) {
 		subscriptions.push(this);
 		this.diagnosticCollection = vscode.languages.createDiagnosticCollection();
 
 		vscode.workspace.onDidOpenTextDocument(this.lint, this, subscriptions);
-		vscode.workspace.onDidCloseTextDocument((textDocument)=> {
+		vscode.workspace.onDidCloseTextDocument((textDocument) => {
 			this.diagnosticCollection.delete(textDocument.uri);
 		}, null, subscriptions);
 
 		vscode.workspace.onDidSaveTextDocument((textDocument: vscode.TextDocument) => {
-				this.diagnosticCollection.delete(textDocument.uri);
-				this.lint(textDocument);
+			this.diagnosticCollection.delete(textDocument.uri);
+			this.lint(textDocument);
 		}, this);
 	}
-	
+
 	public dispose(): void {
 		this.diagnosticCollection.clear();
-		this.diagnosticCollection.dispose();		
+		this.diagnosticCollection.dispose();
 	}
 }
